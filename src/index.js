4dcfcfa674aca298e5a3ed6f17e29784f93a05ba
tio01 <yagoraio2007@gmail.com>
2025-08-16 18:51:16 -0300

    primeiro codigo

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ if (xp <= 1000) return "bronze";
 else if (xp <=2000) return "prata";
 else if (xp <= 3000) return "ouro";
 else if (xp <= 4000) return "platina";
+else {
+    return "challenger"
+}
 }
 while (xp <=maxxp){
  console.log(heroi, "atacou", "o", vilao, "e ganhou", dano, "de xp")
