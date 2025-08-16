53dbc4af3f539243b62c1b4559b080b69ba67970
tio01 <yagoraio2007@gmail.com>
2025-08-16 18:41:04 -0300

    meu primeiro codigo

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let heroi = "link"
+let vilao = "ganon"
+let xp = 0
+let maxxp= 4000
+let dano = 500
+function rank(xp){
+if (xp <= 1000) return "bronze";
+else if (xp <=2000) return "prata";
+else if (xp <= 3000) return "ouro";
+else if (xp <= 4000) return "platina";
+}
+while (xp <=maxxp){
+ console.log(heroi, "atacou", "o", vilao, "e ganhou", dano, "de xp")
+  xp += dano
+  console.log("agora o", heroi, "tem", xp)
+  	console.log (rank(xp), "de", heroi)
+  	}
